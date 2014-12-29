afd65bf688882ac42f62171642e3d1c0f381b5f6
Intel <chemicstry@gmail.com>
2014-12-29 02:54:44 +0200

    Core/Loot: Implemented CMSG_LOOT packet
    (cherry picked from commit 821e2534607b36f401f12fe9f9195b01b133d911)

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -230,6 +230,11 @@ namespace WorldPackets
         class LFGLeave;
     }
 
+    namespace Loot
+    {
+        class LootUnit;
+    }
+
     namespace Mail
     {
         class MailCreateTextItem;
@@ -775,7 +780,7 @@ class TC_GAME_API WorldSession
         void HandleRepopRequest(WorldPackets::Misc::RepopRequest& packet);
         void HandleAutostoreLootItemOpcode(WorldPacket& recvPacket);
         void HandleLootMoneyOpcode(WorldPacket& recvPacket);
-        void HandleLootOpcode(WorldPacket& recvPacket);
+        void HandleLootOpcode(WorldPackets::Loot::LootUnit& packet);
         void HandleLootReleaseOpcode(WorldPacket& recvPacket);
         void HandleLootMasterGiveOpcode(WorldPacket& recvPacket);
         void HandleWhoOpcode(WorldPacket& recvPacket);
