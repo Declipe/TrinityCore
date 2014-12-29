d876d150c35dc494478e83ffef465f191dfcc0d8
joschiwald <joschiwald.trinity@gmail.com>
2014-12-29 01:00:16 +0100

    Core/Scripts: multiple changes
    - fixed non pch build
    - fixed some warnings
    - fixed some coverity issues
    - some random things here and there
    
    (cherry picked from commit 9ecc578cb187cc1ae0fd454883dab0cd058d3807)

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -1596,7 +1596,7 @@ class spell_ethereal_pet_aura : public AuraScript
     {
         PreventDefaultAction();
 
-        std::list<Creature*> minionList;
+        std::list<TempSummon*> minionList;
         GetUnitOwner()->GetAllMinionsByEntry(minionList, NPC_ETHEREAL_SOUL_TRADER);
         for (Creature* minion : minionList)
         {
@@ -2529,6 +2529,8 @@ class spell_gen_mounted_charge : public SpellScript
                 }
                 break;
             }
+            default:
+                break;
         }
     }
 
