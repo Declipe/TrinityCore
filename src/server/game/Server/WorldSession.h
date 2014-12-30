0ea15b2274ff7c07f67b0ab4e76b9423fc633016
Intel <chemicstry@gmail.com>
2014-12-30 03:09:57 +0200

    Core/Packets: Added CMSG_AREATRIGGER
    (cherry picked from commit f64e227e22e564c496cd795f78f4bdc59813f2b5)

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -80,6 +80,11 @@ class RBACData;
 
 namespace WorldPackets
 {
+    namespace AreaTrigger
+    {
+        class AreaTrigger;
+    }
+
     namespace Bank
     {
         class AutoBankItem;
@@ -818,7 +823,7 @@ class TC_GAME_API WorldSession
         void HandleSetAmmoOpcode(WorldPacket& recvPacket);
         void HandleItemNameQueryOpcode(WorldPacket& recvPacket);
 
-        void HandleAreaTriggerOpcode(WorldPacket& recvPacket);
+        void HandleAreaTriggerOpcode(WorldPackets::AreaTrigger::AreaTrigger& packet);
 
         void HandleSetFactionAtWar(WorldPacket& recvData);
         void HandleSetFactionCheat(WorldPacket& recvData);
