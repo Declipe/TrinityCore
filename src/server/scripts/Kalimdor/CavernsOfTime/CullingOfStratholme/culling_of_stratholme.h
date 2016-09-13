68ec2d253064d1ad13c6ba1e4ad6556ddd5ae7a1
treeston <treeston.mmoc@gmail.com>
2016-09-14 01:40:42 +0200

    Add "A developer's guide to finding your way around this instance script" overview section to culling_of_stratholme.h - hopefully one of the first places any maintenance dev will look.

--- src/server/scripts/Kalimdor/CavernsOfTime/CullingOfStratholme/culling_of_stratholme.h
+++ src/server/scripts/Kalimdor/CavernsOfTime/CullingOfStratholme/culling_of_stratholme.h
@@ -20,6 +20,72 @@
 
 #include "Creature.h"
 
+/***********************************************************************************************************************\
+|************************* A DEVELOPER'S GUIDE TO FINDING YOUR WAY AROUND THIS INSTANCE SCRIPT *************************|
+|***********************************************************************************************************************|
+*   The primary instance logic is split between the instance control script (instance_culling_of_stratholme.cpp) and    *
+*   Arthas' creature AI (npc_arthas.cpp). The instance script is responsible for global state management and control    *
+*   of the Scourge Wave event, as well as spawn control (ties into state management) with the help of the templated     *
+*   CreatureScript wrapper (CanSpawn override as well as Despawn checks on state change) defined in this very header.   *
+*   Arthas handles all the actual RP timings, pathing, spawns, casts and everything else, really. He also does tons     *
+*   of state control for himself (positional snapback and gossip/flag management).                                      *
+*   Communication between these two scripts (with some auxiliary other scripts throwing stuff in too) is handled        *
+*   via the SetData/SetGuidData/SetGUID methods on CreatureAI and InstanceScript (as appropriate), as well as           *
+*   data retrieval using GetData (instance states and the like).                                                        *
+*   All values specified in this header find use across multiple files (used for communication between them) while      *
+*   the enums in the files themselves contain action IDs, event IDs, entries and whatnot that only find use in that     *
+*   particular file.                                                                                                    *
+*   Generally, any RP sequence will follow the form:                                                                    *
+*    1. RP sequence is started (creature gossip sends SetGuidData with starting player GUID to InstanceScript)          *
+*    2. InstanceScript verifies state consistency and advances the global instance state                                *
+*    3. InstanceScript passes the player GUID to Arthas AI using SetGUID. The int32 data type serves as action ID.      *
+*    4. Arthas AI schedules all necessary events for the sequence and handles it from there using its UpdateAI loop.    *
+*    5. At the end of the sequence, Arthas AI notifies the instance script (using SetData).                             *
+*    6. The instance script advances the global state again. This state change is propagated to Arthas AI (SetData).    *
+*    7. Arthas AI is notified of a state change, gets current progress (GetData) and adjusts gossip availability etc.   *
+*    8. The cycle repeats. RP sequences (as above) can (and do) also include combat sequences and similar.              *
+|***********************************************************************************************************************|
+|*********************************************** KEY METHODS TO LOOK FOR ***********************************************|
+|***********************************************************************************************************************|
+*   Arthas AI: (npc_arthas_stratholmeAI in npc_arthas.cpp)                                                              *
+*    - AdvanceToState:                                                                                                  *
+*      - handles all the state updating/position consistency logic                                                      *
+*      - invoked from SetData when the instance script notifies Arthas of a state change                                *
+*    - MoveAlongPath:                                                                                                   *
+*      - handles RP sequence movement                                                                                   *
+*      - takes two indices for the master _positions array (start + end) and moves a creature along the WPs between     *
+*    - ScheduleActionOOC:                                                                                               *
+*      - queues the specified action to be passed to DoAction after Arthas finishes combat (JustReachedHome)            *
+*      - typically called during RP sequences just after ensuring Arthas has entered combat with all enemies involved   *
+*    - MovementInform:                                                                                                  *
+*      - handles movement notifications not just for Arthas, but also some other creatures that run RP slave AI         *
+*      - all movement IDs are indices of the one single _positions array, which means they are still unique             *
+*    - EnterCombat:                                                                                                     *
+*      - finds out where along the path we currently are, and stores that so we can resume after (JustReachedHome)      *
+|***********************************************************************************************************************|
+*   Instance script: (instance_culling_of_stratholme in instance_culling_of_stratholme.cpp)                             *
+*    - GetStableStateFor:                                                                                               *
+*      - handles state regression; only certain states should be loaded after a wipe/soft reset                         *
+*    - SetInstanceProgress:                                                                                             *
+*      - any changing of the global instance state is done through this method                                          *
+*      - handles world state adjustments, spawn control (through AI notification) as well as reset behavior             *
+*    - SetWorldState/PropagateWorldStateUpdate:                                                                         *
+*      - the instance script stores a std::map of all current world state values for the instance                       *
+*      - SetWorldState adjusts this map, PropagateWorldStateUpdate sends any changed values to all players              *
+*      - FillInitialWorldStates also draws upon this map to ensure all players have the same states at all times        *
+*    - CreatureAIHello/CreatureAIGoodbye:                                                                               *
+*      - Ties into the creature script wrapper (defined further down in this file)                                      *
+*      - CAIHello registers a creature as part of the progress phases specified, allowing the instance script to        *
+*        notify it of state changes away from that state (so it can properly despawn if appropriate).                   *
+|***********************************************************************************************************************|
+*   Spawn control script: (StratholmeCreatureScript<ParentAI> in culling_of_stratholme.h)                               *
+*    - CanSpawn override:                                                                                               *
+*      - gets current instance state using GetData, then checks if we should be allowed to respawn (bitmask check)      *
+*    - StratholmeNPCAIWrapper::CheckDespawn:                                                                            *
+*      - gets current instance state using GetData, then checks if we should despawn ourselves (bitmask check)          *
+\***********************************************************************************************************************/
+
+
 // Note: These are bitmask values to allow combining (spawn state masks on template AI), but only a single bit will ever be true in instance script
 enum ProgressStates : uint32
 {
