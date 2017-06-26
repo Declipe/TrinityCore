0cef798b59077c354c9f87154ec1f4eaa05bd1cf
Declipe <Declipe@yandex.ru>
2017-06-26 15:54:39 +0300

    Merge branch '3.3.5' of https://github.com/TrinityCore/TrinityCore into 3.3.5

---
+++ sql/TrinityCore-Patches/beta.sql
@@ -0,0 +1,7 @@
+UPDATE `item_template` SET `displayid` = 28541 WHERE (entry = 2567);
+UPDATE `item_template` SET `displayid` = 28541 WHERE (entry = 3413);
+UPDATE `item_template` SET `displayid` = 44850 WHERE (entry = 3417);
+UPDATE `item_template` SET `displayid` = 45086 WHERE (entry = 3902);
+UPDATE `item_template` SET `displayid` = 55920 WHERE (entry = 6472);
+UPDATE `item_template` SET `displayid` = 31301 WHERE (entry = 7710);
+UPDATE `item_template` SET `displayid` = 30993 WHERE (entry = 7726);
