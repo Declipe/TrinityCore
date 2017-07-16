a4df240207590d8fb7be599475bc5c1e5109d63b
Declipe <Declipe@yandex.ru>
2017-07-16 17:00:08 +0300

    Merge branch '3.3.5' of https://github.com/TrinityCore/TrinityCore into 3.3.5
    # Пожалуйста, введите сообщение коммита, для объяснения, зачем нужно
    # это слияние, особенно, если это слияние обновленной вышестоящей
    # ветки в тематическую ветку.
    #
    # Строки, начинающиеся с «#» будут проигнорированы, а пустое
    # сообщение отменяет процесс коммита.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -22706,6 +22706,7 @@ void Player::SendInitialPacketsAfterAddToMap()
     SendAurasForTarget(this);
     SendEnchantmentDurations();                             // must be after add to map
     SendItemDurations();                                    // must be after add to map
+    SendQuestGiverStatusMultiple();
 
     // raid downscaling - send difficulty to player
     if (GetMap()->IsRaid())
