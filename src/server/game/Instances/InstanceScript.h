f121d35e858b9a95152d2c31030876e7adb78e99
Declipe <Declipe@yandex.ru>
2021-03-02 06:59:22 +0300

    Revert "Revert "Revert "update guild level"""
    This reverts commit 977769e130a39857243303dc6db9d9773d0898d8.

--- src/server/game/Instances/InstanceScript.h
+++ src/server/game/Instances/InstanceScript.h
@@ -259,8 +259,6 @@ class TC_GAME_API InstanceScript : public ZoneScript
 
         virtual void FillInitialWorldStates(WorldPackets::WorldState::InitWorldStates& /*packet*/) { }
 
-        void givexp();
-
         uint32 GetEncounterCount() const { return bosses.size(); }
 
         // Only used by areatriggers that inherit from OnlyOnceAreaTriggerScript
