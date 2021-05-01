b4fed156e24defee7b15e3503a206470e8372999
Declipe <Declipe@yandex.ru>
2021-05-02 01:15:47 +0300

    Revert "sd"
    This reverts commit 287a2ab6642c6e5fa68c44ddbbd094ab8e92e85b.

--- src/server/game/Achievements/AchievementMgr.cpp
+++ src/server/game/Achievements/AchievementMgr.cpp
@@ -280,7 +280,7 @@ bool AchievementCriteriaData::IsValid(AchievementCriteriaEntry const* criteria)
             }
             return true;
         case ACHIEVEMENT_CRITERIA_DATA_TYPE_S_KNOWN_TITLE:
-            if (!sDBCMgr->GetCharTitlesEntry(known_title.title_id))
+            if (!sCharTitlesStore.LookupEntry(known_title.title_id))
             {
                 TC_LOG_ERROR("sql.sql", "Table `achievement_criteria_data` (Entry: %u Type: %u) for data type ACHIEVEMENT_CRITERIA_DATA_TYPE_S_KNOWN_TITLE (%u) contains an unknown title_id in value1 (%u), ignore.",
                     criteria->ID, criteria->Type, dataType, known_title.title_id);
@@ -450,7 +450,7 @@ bool AchievementCriteriaData::Meets(uint32 criteria_id, Player const* source, Wo
         }
         case ACHIEVEMENT_CRITERIA_DATA_TYPE_S_KNOWN_TITLE:
         {
-            if (CharTitlesEntry const* titleInfo = sDBCMgr->GetCharTitlesEntry(known_title.title_id))
+            if (CharTitlesEntry const* titleInfo = sCharTitlesStore.LookupEntry(known_title.title_id))
                 return source->HasTitle(titleInfo->MaskID);
             return false;
         }
@@ -620,8 +620,7 @@ void AchievementMgr::LoadFromDB(PreparedQueryResult achievementResult, PreparedQ
             // title achievement rewards are retroactive
             if (AchievementReward const* reward = sAchievementMgr->GetAchievementReward(achievement))
                 if (uint32 titleId = reward->TitleId[Player::TeamForRace(GetPlayer()->GetRace()) == ALLIANCE ? 0 : 1])
-                  //  if (CharTitlesEntry const* titleEntry = sCharTitlesStore.LookupEntry(titleId))
-                    if (CharTitlesEntry const* titleEntry = sDBCMgr->GetCharTitlesEntry(titleId))
+                    if (CharTitlesEntry const* titleEntry = sCharTitlesStore.LookupEntry(titleId))
                         GetPlayer()->SetTitle(titleEntry);
 
         } while (achievementResult->NextRow());
@@ -1537,8 +1536,7 @@ void AchievementMgr::CompletedAchievement(AchievementEntry const* achievement)
     //! we explicitly check by ID. Maybe in the future we could move the achievement_reward
     //! condition fields to the condition system.
     if (uint32 titleId = reward->TitleId[achievement->ID == 1793 ? GetPlayer()->GetNativeGender() : (GetPlayer()->GetTeam() == ALLIANCE ? 0 : 1)])
-       // if (CharTitlesEntry const* titleEntry = sCharTitlesStore.LookupEntry(titleId))
-        if (CharTitlesEntry const* titleEntry = sDBCMgr->GetCharTitlesEntry(titleId))
+        if (CharTitlesEntry const* titleEntry = sCharTitlesStore.LookupEntry(titleId))
             GetPlayer()->SetTitle(titleEntry);
 
     // mail
@@ -2635,8 +2633,7 @@ void AchievementGlobalMgr::LoadRewards()
 
         if (reward.TitleId[0])
         {
-            //CharTitlesEntry const* titleEntry = sCharTitlesStore.LookupEntry(reward.TitleId[0]);
-            CharTitlesEntry const* titleEntry = sDBCMgr->GetCharTitlesEntry(reward.TitleId[0]);
+            CharTitlesEntry const* titleEntry = sCharTitlesStore.LookupEntry(reward.TitleId[0]);
             if (!titleEntry)
             {
                 TC_LOG_ERROR("sql.sql", "Table `achievement_reward` (Entry: %u) contains an invalid title id (%u) in `title_A`, set to 0", id, reward.TitleId[0]);
@@ -2646,8 +2643,7 @@ void AchievementGlobalMgr::LoadRewards()
 
         if (reward.TitleId[1])
         {
-            //CharTitlesEntry const* titleEntry = sCharTitlesStore.LookupEntry(reward.TitleId[1]);
-            CharTitlesEntry const* titleEntry = sDBCMgr->GetCharTitlesEntry(reward.TitleId[1]);
+            CharTitlesEntry const* titleEntry = sCharTitlesStore.LookupEntry(reward.TitleId[1]);
             if (!titleEntry)
             {
                 TC_LOG_ERROR("sql.sql", "Table `achievement_reward` (Entry: %u) contains an invalid title id (%u) in `title_H`, set to 0", id, reward.TitleId[1]);
--- src/server/scripts/Commands/cs_titles.cpp
+++ src/server/scripts/Commands/cs_titles.cpp
@@ -85,7 +85,7 @@ public:
         if (handler->HasLowerSecurity(target, ObjectGuid::Empty))
             return false;
 
-        CharTitlesEntry const* titleInfo = sDBCMgr->GetCharTitlesEntry(id);
+        CharTitlesEntry const* titleInfo = sCharTitlesStore.LookupEntry(id);
         if (!titleInfo)
         {
             handler->PSendSysMessage(LANG_INVALID_TITLE_ID, id);
@@ -130,7 +130,7 @@ public:
         if (handler->HasLowerSecurity(target, ObjectGuid::Empty))
             return false;
 
-        CharTitlesEntry const* titleInfo = sDBCMgr->GetCharTitlesEntry(id);
+        CharTitlesEntry const* titleInfo = sCharTitlesStore.LookupEntry(id);
         if (!titleInfo)
         {
             handler->PSendSysMessage(LANG_INVALID_TITLE_ID, id);
@@ -176,7 +176,7 @@ public:
         if (handler->HasLowerSecurity(target, ObjectGuid::Empty))
             return false;
 
-        CharTitlesEntry const* titleInfo = sDBCMgr->GetCharTitlesEntry(id);
+        CharTitlesEntry const* titleInfo = sCharTitlesStore.LookupEntry(id);
         if (!titleInfo)
         {
             handler->PSendSysMessage(LANG_INVALID_TITLE_ID, id);
@@ -226,9 +226,8 @@ public:
 
         uint64 titles2 = titles;
 
-        //for (uint32 i = 1; i < sCharTitlesStore.GetNumRows(); ++i)
-        for (uint32 i = 1; i < sDBCMgr->CharTitlesStore.size(); ++i)
-            if (CharTitlesEntry const* tEntry = sDBCMgr->GetCharTitlesEntry(i))
+        for (uint32 i = 1; i < sCharTitlesStore.GetNumRows(); ++i)
+            if (CharTitlesEntry const* tEntry = sCharTitlesStore.LookupEntry(i))
                 titles2 &= ~(uint64(1) << tEntry->MaskID);
 
         titles &= ~titles2;                                     // remove non-existing titles
