32dd441aa0b874e694c3f6d86a73ada2eec9ef02
Shauren <shauren.trinity@gmail.com>
2022-07-03 22:22:45 +0200

    Revert "Core/Event: prevent seasonal quests from resetting at server restart (#17455)"
    This reverts commit 6a8ca9b0bf6f7cd355e1994723d6be6a5003d4d4.
    
    (cherry picked from commit 306022054bec6010a63e80407c8d2f8d672b935b)

--- src/server/game/Events/GameEventMgr.cpp
+++ src/server/game/Events/GameEventMgr.cpp
@@ -145,8 +145,6 @@ bool GameEventMgr::StartEvent(uint16 event_id, bool overwrite)
                 data.end = data.start + data.length * MINUTE;
         }
 
-        // When event is started, set its worldstate to current time
-        sWorld->setWorldState(event_id, GameTime::GetGameTime());
 #ifdef ELUNA
         if (IsActiveEvent(event_id))
             if (Eluna* e = sWorld->GetEluna())
@@ -191,9 +189,6 @@ void GameEventMgr::StopEvent(uint16 event_id, bool overwrite)
 
     RemoveActiveEvent(event_id);
     UnApplyEvent(event_id);
-
-    // When event is stopped, clean up its worldstate
-    sWorld->setWorldState(event_id, 0);
 
     if (overwrite && !serverwide_evt)
     {
@@ -1125,8 +1120,6 @@ uint32 GameEventMgr::Update()                               // return the next e
         }
         else
         {
-            // If event is inactive, periodically clean up its worldstate
-            sWorld->setWorldState(itr, 0);
             //TC_LOG_DEBUG("misc", "GameEvent {} is not active", itr->first);
             if (IsActiveEvent(itr))
                 deactivate.insert(itr);
@@ -1210,10 +1203,8 @@ void GameEventMgr::ApplyNewEvent(uint16 event_id)
     //! Run SAI scripts with SMART_EVENT_GAME_EVENT_START
     RunSmartAIScripts(event_id, true);
 
-    // If event's worldstate is 0, it means the event hasn't been started yet. In that case, reset seasonal quests.
-    // When event ends (if it expires or if it's stopped via commands) worldstate will be set to 0 again, ready for another seasonal quest reset.
-    if (sWorld->getWorldState(event_id) == 0)
-        sWorld->ResetEventSeasonalQuests(event_id);
+    // check for seasonal quest reset.
+    sWorld->ResetEventSeasonalQuests(event_id);
 }
 
 void GameEventMgr::UpdateEventNPCFlags(uint16 event_id)
