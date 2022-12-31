0141158be50a927b3394c7324c0767c934ae5064
Declipe <Declipe@yandex.ru>
2022-12-31 17:57:51 +0300

    1

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1785,6 +1785,8 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
         void DurabilityPointLossForEquipSlot(EquipmentSlots slot);
         uint32 DurabilityRepairAll(bool cost, float discountMod, bool guildBank);
         uint32 DurabilityRepair(uint16 pos, bool cost, float discountMod, bool guildBank);
+        //void DurabilityRepairAll(bool takeCost, float discountMod, bool guildBank);
+        //void DurabilityRepair(uint16 pos, bool takeCost, float discountMod);
 
         void UpdateMirrorTimers();
         void StopMirrorTimers();
