63a434f9a0271b1f5ed99caa7d2025b62b8eeb6f
Declipe <Declipe@yandex.ru>
2023-01-23 21:13:22 +0300

    rev.

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -59,7 +59,6 @@ void AddSC_Mod_ItemUpgrade();
 void AddSC_Mod_AutoLearn();
 void AddSC_Transmogrification();
 void AddSC_mod_DefaultSpec();
-void AddSC_npc_buffer2();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
@@ -111,5 +110,4 @@ void AddCustomScripts()
     AddSC_dk_boss_black_knight();
     AddSC_dk_instance_trial_of_the_champion();
     AddSC_dk_boss_argent_challenge();
-    AddSC_npc_buffer2();
 }
