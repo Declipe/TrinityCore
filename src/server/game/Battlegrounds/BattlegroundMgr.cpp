ae79bd63fe4567d818d2b33890f092767b0138fb
Dmitry <Declipe@yandex.ru>
2023-07-18 21:52:01 +0300

    update "battlemasterlist dbc"

--- src/server/game/Battlegrounds/BattlegroundMgr.cpp
+++ src/server/game/Battlegrounds/BattlegroundMgr.cpp
@@ -33,6 +33,7 @@
 #include "Chat.h"
 #include "DatabaseEnv.h"
 #include "DisableMgr.h"
+#include "DBCStores.h"
 #include "Formulas.h"
 #include "GameEventMgr.h"
 #include "GameTime.h"
@@ -542,7 +543,7 @@ void BattlegroundMgr::LoadBattlegroundTemplates()
             continue;
 
         // can be overwrite by values from DB
-        BattlemasterListEntry const* bl = sBattlemasterListStore.LookupEntry(bgTypeId);
+        BattlemasterListEntry const* bl = sDBCMgr->GetBattlemasterListEntry(bgTypeId);
         if (!bl)
         {
             TC_LOG_ERROR("bg.battleground", "Battleground ID %u could not be found in BattlemasterList.dbc. The battleground was not created.", bgTypeId);
@@ -904,7 +905,7 @@ void BattlegroundMgr::LoadBattleMastersEntry()
         }
 
         uint32 bgTypeId  = fields[1].GetUInt32();
-        if (!sBattlemasterListStore.LookupEntry(bgTypeId))
+        if (!sDBCMgr->GetBattlemasterListEntry(bgTypeId))
         {
             TC_LOG_ERROR("sql.sql", "Table `battlemaster_entry` contains entry %u for a non-existing battleground type %u, ignored.", entry, bgTypeId);
             continue;
