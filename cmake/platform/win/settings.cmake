c6e3ffc1bda9e807d668e42c6f1235b31cf1d885
Declipe <Declipe@yandex.ru>
2023-10-24 19:56:51 +0300

    up

--- cmake/platform/win/settings.cmake
+++ cmake/platform/win/settings.cmake
@@ -1,6 +1,8 @@
 add_definitions(-D_WIN32_WINNT=0x0601)
 add_definitions(-DWIN32_LEAN_AND_MEAN)
 add_definitions(-DNOMINMAX)
+set(ACE_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/acelite)
+set(ACE_LIBRARY "ace")
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   include(${CMAKE_SOURCE_DIR}/cmake/compiler/msvc/settings.cmake)
