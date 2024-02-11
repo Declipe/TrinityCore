6da1466f85a9239497d140add12fdf0a02d78f72
Dmitry <Declipe@yandex.ru>
2024-02-11 16:14:47 +0300

    1

---
+++ sql/custom Dkcore/custom fix/update diff.sql
@@ -0,0 +1,2 @@
+INSERT INTO `creature_template2_difficulty` (`CreatureId`, `DifficultyCreatureId1`, `DifficultyCreatureId2`, `DifficultyCreatureId3`) 
+SELECT `entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3` FROM `creature_template2`;
