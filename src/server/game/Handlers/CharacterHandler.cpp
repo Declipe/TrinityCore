b73a6b5853b6148bf81bee84c374a33305db2b18
Declipe <Declipe@yandex.ru>
2024-05-07 22:46:27 +0300

    UPDATE

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -827,7 +827,7 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
 
             // send new char string if not empty
             if (!sWorld->GetNewCharString().empty())
-                chH.PSendSysMessage("{}", sWorld->GetNewCharString().c_str());
+                chH.PSendSysMessage("%s", sWorld->GetNewCharString().c_str());
         }
     }
 
