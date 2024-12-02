0299d330de81bc556e5eafbde7ce0324aa69193f
Shauren <shauren.trinity@gmail.com>
2024-12-02 15:19:19 +0100

    Core/Conditions: Added missing CONDITION_STRING_ID case to Condition::GetSearcherTypeMaskForCondition
    (cherry picked from commit 7153538db65a2a78bd16339ea44025a9af5fc27c)

--- src/server/game/Conditions/ConditionMgr.cpp
+++ src/server/game/Conditions/ConditionMgr.cpp
@@ -754,6 +754,9 @@ uint32 Condition::GetSearcherTypeMaskForCondition() const
         case CONDITION_GUILD_LEVEL:
             mask |= GRID_MAP_TYPE_MASK_PLAYER;
             break;
+        case CONDITION_STRING_ID:
+            mask |= GRID_MAP_TYPE_MASK_CREATURE | GRID_MAP_TYPE_MASK_GAMEOBJECT;
+            break;
         default:
             ABORT_MSG("Condition::GetSearcherTypeMaskForCondition - missing condition handling!");
             break;
