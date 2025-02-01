04a20e68cb84f88c629844442457fbb9db1f5a6e
Declipe <Declipe@yandex.ru>
2025-02-01 22:47:09 +0300

    Revert "test crash"
    This reverts commit ffd1cdf84548992ea39fb2bee33e66009887d594.

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -4348,15 +4348,10 @@ bool InstanceMap::HasPermBoundPlayers() const
 uint32 InstanceMap::GetMaxPlayers() const
 {
     MapDifficulty const* mapDiff = GetMapDifficulty();
-    if (mapDiff && mapDiff->maxPlayers && sConfigMgr->GetBoolDefault("AutoBalance.enable", true))
-    {
+    if (mapDiff && mapDiff->maxPlayers && (sConfigMgr->GetBoolDefault("AutoBalance.enable", true)))
         return (mapDiff->maxPlayers == 10 ? 30 : mapDiff->maxPlayers);
-    }
-
-    if (mapDiff)
-    {
+    else
         return mapDiff->maxPlayers;
-    }
 
     return GetEntry()->MaxPlayers;
 }
