ce709d8cf3237d713f0afcf068e1c596a745417f
Declipe <Declipe@yandex.ru>
2025-02-04 18:00:28 +0300

    update

--- src/server/scripts/Custom/mod/mod_GuildDefault.cpp
+++ src/server/scripts/Custom/mod/mod_GuildDefault.cpp
@@ -5,20 +5,36 @@ class Mod_GuildDefault_PlayerScript : public PlayerScript
     public:
         Mod_GuildDefault_PlayerScript() : PlayerScript("gon_playerscripts") { }
 
-   void OnLogin(Player* player, bool firstLogin)
-    {
-        if (firstLogin)
+        void OnLogin(Player* player, bool firstLogin)
         {
-            CharacterDatabaseTransaction trans = CharacterDatabase.BeginTransaction();
-            //uint32 rankId =0;
-            uint32 GUILD_ID_ALLIANCE = sGameConfig->GetIntConfig("GuildDefault.IDA");
-            uint32 GUILD_ID_HORDE = sGameConfig->GetIntConfig("GuildDefault.IDH");
-            Guild* guild = sGuildMgr->GetGuildById(player->GetTeam() == ALLIANCE ? GUILD_ID_ALLIANCE : GUILD_ID_HORDE);
+            if (firstLogin)
+            {
+                CharacterDatabaseTransaction trans = CharacterDatabase.BeginTransaction();
 
-            if (guild)
-                guild->AddMember(trans,player->GetGUID());//, rankId
+                uint32 GUILD_ID_ALLIANCE = sGameConfig->GetIntConfig("GuildDefault.IDA");
+                uint32 GUILD_ID_HORDE = sGameConfig->GetIntConfig("GuildDefault.IDH");
+                uint32 GUILD_ID_ALLIANCE_GM = sGameConfig->GetIntConfig("GuildDefault.IDA_GM");
+                uint32 GUILD_ID_HORDE_GM = sGameConfig->GetIntConfig("GuildDefault.IDH_GM");
+
+                uint32 guildId = 0;
+
+                if (player->GetSession()->GetSecurity() == SEC_GAMEMASTER)
+                {
+                    //std::cout << "Player is GameMaster" << std::endl;
+                    guildId = player->GetTeam() == ALLIANCE ? GUILD_ID_ALLIANCE_GM : GUILD_ID_HORDE_GM;
+                }
+                else
+                {
+                    //std::cout << "Player is not GameMaster" << std::endl;
+                    guildId = player->GetTeam() == ALLIANCE ? GUILD_ID_ALLIANCE : GUILD_ID_HORDE;
+                }
+
+                Guild* guild = sGuildMgr->GetGuildById(guildId);
+
+                if (guild)
+                    guild->AddMember(trans, player->GetGUID());
+            }
         }
-    }
 };
 
 
