3fc01b24bfa2487d749c39446e322c8ea649f1b7
Declipe <Declipe@yandex.ru>
2025-02-25 17:25:20 +0300

    update

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -18,6 +18,7 @@
 #include "Map.h"
 #include "Battleground.h"
 #include "CellImpl.h"
+#include "CustomConfig.h"
 #include "Config.h"
 #include "Chat.h"
 #include "DatabaseEnv.h"
@@ -2888,12 +2889,12 @@ float Map::GetWaterLevel(float x, float y) const
 
 bool Map::isInLineOfSight(float x1, float y1, float z1, float x2, float y2, float z2, uint32 phasemask, LineOfSightChecks checks, VMAP::ModelIgnoreFlags ignoreFlags) const
 {
-    if (sWorld->getBoolConfig(CONFIG_VMAP_ENHANCED_LOS_PVP) && IsBattlegroundOrArena())
+    if (sGameConfig->GetBoolConfig("vmap.EnhancedPvPLOS") && IsBattlegroundOrArena())
     {
         ignoreFlags = VMAP::ModelIgnoreFlags::Nothing;
     }
 
-    if (sWorld->getBoolConfig(CONFIG_VMAP_ENHANCED_LOS_WORLD) && IsWorldMap())
+    if (sGameConfig->GetBoolConfig("vmap.EnhancedWorldLOS") && IsWorldMap())
     {
         ignoreFlags = VMAP::ModelIgnoreFlags::Nothing;
     }
