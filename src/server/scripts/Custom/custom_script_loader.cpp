ee60bf65fb139e616701484db3ba4dc883969c04
Declipe <Declipe@yandex.ru>
2025-03-23 09:25:20 +0300

    update

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -16,6 +16,7 @@
  */
 
 // This is where scripts' loading functions should be declared:
+void AddSC_GOMove_commandscript();
 void AddSC_RebootBuffOnPlayerLogin();
 void AddAccountAchievementsScripts();
 void AddSC_item_upgrade_worldscript();
@@ -143,4 +144,5 @@ void AddCustomScripts()
     AddSC_item_upgrade_itemscript();
     AddAccountAchievementsScripts();
     AddSC_RebootBuffOnPlayerLogin();
+    AddSC_GOMove_commandscript();
 }
