d040230233b0e3cdb5c51847c38c293f9ef8367d
Declipe <Declipe@yandex.ru>
2025-05-24 20:20:58 +0300

    Core/Players: Fix Titan's Grip weapon type restrictions

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1183,7 +1183,7 @@ public:
 
     void SetVirtualItemSlot(uint8 i, Item* item);
     void SetSheath(SheathState sheathed) override;             // overwrite Unit version
-    uint8 FindEquipSlot(ItemTemplate const* proto, uint32 slot, bool swap) const;
+    uint8 FindEquipSlot(Item const* item, uint32 slot, bool swap) const;
     uint32 GetItemCount(uint32 item, bool inBankAlso = false, Item* skipItem = nullptr) const;
     uint32 GetItemCountWithLimitCategory(uint32 limitCategory, Item* skipItem = nullptr) const;
     Item* GetItemByGuid(ObjectGuid guid) const;
@@ -1980,8 +1980,8 @@ public:
     void SetCanParry(bool value);
     bool CanBlock() const { return m_canBlock; }
     void SetCanBlock(bool value);
-    bool CanTitanGrip() const { return m_canTitanGrip; }
-    void SetCanTitanGrip(bool value, uint32 penaltySpellId = 0);
+    bool CanTitanGrip(Item const* item) const;
+    void SetCanTitanGrip(bool value, uint32 penaltySpellId = 0, int32 allowedItemClass = 0, int32 allowedItemSubClassMask = 0);
     void CheckTitanGripPenalty();
     bool CanTameExoticPets() const { return IsGameMaster() || HasAuraType(SPELL_AURA_ALLOW_TAME_PET_TYPE); }
 
@@ -2590,6 +2590,8 @@ protected:
     bool m_canParry;
     bool m_canBlock;
     bool m_canTitanGrip;
+    uint32 m_titanGripWeaponSubclasses;
+    uint32 m_titanGripArmorSubclasses;
     uint32 m_titanGripPenaltySpellId;
     uint8 m_swingErrorMsg;
     float m_ammoDPS;
