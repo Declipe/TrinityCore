9a495be0842858b4aa164dc62526f13974ed0b3f
Declipe <Declipe@yandex.ru>
2025-05-29 21:53:25 +0300

    sql

---
+++ sql/custom Dkcore/mod/auth/auth hardcore.sql
@@ -0,0 +1,11 @@
+INSERT INTO `rbac_permissions` (`id`, `name`) VALUES (4008, '1');
+INSERT INTO `rbac_permissions` (`id`, `name`) VALUES (4009, '1');
+INSERT INTO `rbac_permissions` (`id`, `name`) VALUES (4010, '1');
+INSERT INTO `rbac_permissions` (`id`, `name`) VALUES (4011, '1');
+INSERT INTO `rbac_permissions` (`id`, `name`) VALUES (4012, '1');
+
+INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) VALUES (195, 4008);
+INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) VALUES (195, 4009);
+INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) VALUES (195, 4010);
+INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) VALUES (195, 4011);
+INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) VALUES (195, 4012);
