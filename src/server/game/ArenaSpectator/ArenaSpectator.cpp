40f51bc7644b85731ec9cb7897b0622fd1297334
Declipe <Declipe@yandex.ru>
2025-06-15 00:39:33 +0300

    Revert "test"
    This reverts commit 120d47f459cd52a9ccb1ca4b2fb91d068b3c8111.

--- src/server/game/ArenaSpectator/ArenaSpectator.cpp
+++ src/server/game/ArenaSpectator/ArenaSpectator.cpp
@@ -304,7 +304,7 @@ bool ArenaSpectator::ShouldSendAura(Aura* aura, uint8 effMask, ObjectGuid target
 }
 
 template<>
-TC_GAME_API void ArenaSpectator::SendPacketTo(Player const* player, std::string&& message)
+void ArenaSpectator::SendPacketTo(Player const* player, std::string&& message)
 {
     WorldPacket data;
     CreatePacket(data, message);
@@ -312,7 +312,7 @@ TC_GAME_API void ArenaSpectator::SendPacketTo(Player const* player, std::string&
 }
 
 template<>
-TC_GAME_API void ArenaSpectator::SendPacketTo(const Map* map, std::string&& message)
+ void ArenaSpectator::SendPacketTo(const Map* map, std::string&& message)
 {
     if (!map->IsBattleArena())
         return;
