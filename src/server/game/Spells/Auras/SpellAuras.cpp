74f801e13301bb3920e3da3a5f4149d47e2e6f25
offl <suffle.o@gmail.com>
2025-09-18 18:44:40 +0300

    update

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1793,12 +1793,14 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
     if (apply)
     {
         if (Creature* creature = target->ToCreature())
-            creature->AI()->OnAuraApplied(aurApp);
+            if (CreatureAI* ai = creature->AI())
+                ai->OnAuraApplied(aurApp);
     }
     else
     {
         if (Creature* creature = target->ToCreature())
-            creature->AI()->OnAuraRemoved(aurApp);
+            if (CreatureAI* ai = creature->AI())
+                ai->OnAuraRemoved(aurApp);
     }
 }
 
