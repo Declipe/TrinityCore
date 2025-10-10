8b649293689fc6b10d576c64186617686ac6f34b
Declipe <Declipe@yandex.ru>
2025-10-10 15:58:49 +0300

    Reapply "need test update auctionhousebot"
    This reverts commit c72cafee96db78cec3373c38b11f2e678b16cbe4.

--- src/server/game/Handlers/AuctionHouseHandler.cpp
+++ src/server/game/Handlers/AuctionHouseHandler.cpp
@@ -494,7 +494,6 @@ void WorldSession::HandleAuctionPlaceBid(WorldPacket& recvData)
         GetPlayer()->RemoveAurasByType(SPELL_AURA_FEIGN_DEATH);
 
     AuctionHouseObject* auctionHouse = sAuctionMgr->GetAuctionHouseByFactionTemplateId(creature->GetFaction());
-
     AuctionEntry* auction = auctionHouse->GetAuction(auctionId);
     Player* player = GetPlayer();
 
@@ -644,7 +643,6 @@ void WorldSession::HandleAuctionRemoveItem(WorldPacket& recvData)
         GetPlayer()->RemoveAurasByType(SPELL_AURA_FEIGN_DEATH);
 
     AuctionHouseObject* auctionHouse = sAuctionMgr->GetAuctionHouseByFactionTemplateId(creature->GetFaction());
-
     AuctionEntry* auction = auctionHouse->GetAuction(auctionId);
     Player* player = GetPlayer();
 
@@ -688,7 +686,6 @@ void WorldSession::HandleAuctionRemoveItem(WorldPacket& recvData)
     SendAuctionCommandResult(auction->Id, AUCTION_CANCEL, ERR_AUCTION_OK);
 
     // Now remove the auction
-
     player->SaveInventoryAndGoldToDB(trans);
     auction->DeleteFromDB(trans);
     CharacterDatabase.CommitTransaction(trans);
