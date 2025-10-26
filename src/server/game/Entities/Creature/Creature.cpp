128c5a3f146e2e83871454c9a7e72af7398a5f4b
Shauren <shauren.trinity@gmail.com>
2025-10-26 09:55:59 +0100

    Revert "Core/Entities: improve unit on death movement"
    This reverts commit f4d8105064afdf98212320b5bd1569f2af3fa68a.

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -2039,6 +2039,13 @@ void Creature::setDeathState(DeathState s)
         if (m_formation && m_formation->GetLeader() == this)
             m_formation->FormationReset(true);
 
+        bool needsFalling = (IsFlying() || IsHovering()) && !IsUnderWater();
+        SetHover(false, false);
+        SetDisableGravity(false, false);
+
+        if (needsFalling)
+            GetMotionMaster()->MoveFall();
+
         Unit::setDeathState(CORPSE);
     }
     else if (s == JUST_RESPAWNED)
