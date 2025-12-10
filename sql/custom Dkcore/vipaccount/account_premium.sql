0f1bb67fa9336097e6b30922db0635bd98afc6fa
Declipe <Declipe@yandex.ru>
2025-12-10 17:58:06 +0300

    1

--- sql/custom Dkcore/vipaccount/account_premium.sql
+++ sql/custom Dkcore/vipaccount/account_premium.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `auth3`.`account_premium` (
+CREATE TABLE IF NOT EXISTS `account_premium` (
   `id` int(11) NOT NULL DEFAULT '0' COMMENT 'Account id',
   `setdate` int(4) NOT NULL DEFAULT '0',
   `unsetdate` int(4) NOT NULL DEFAULT '0',
