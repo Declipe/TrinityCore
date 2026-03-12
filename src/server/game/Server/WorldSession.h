jairoe05/tc_duplicate

Core/Packets: CMSG_NAME_QUERY (QueryPlayerName) handler

src/server/game/Server/WorldSession.h
@@ -90,6 +90,7 @@ namespace WorldPackets
         class LogoutRequest;
         class LogoutCancel;
         class LoadingScreenNotify;
+        class QueryPlayerName;
     }
 
     namespace Guild
@@ -452,7 +453,7 @@ class WorldSession
         void HandleCharCreateCallback(PreparedQueryResult result, WorldPackets::Character::CharacterCreateInfo* createInfo);
         void HandlePlayerLoginOpcode(WorldPackets::Character::PlayerLogin& playerLogin);
         void HandleContinuePlayerLogin();
-        void HandleLoadScreenOpcode(WorldPacket& recvPacket);
+        void HandleLoadScreenOpcode(WorldPackets::Character::LoadingScreenNotify& loadingScreenNotify);
         void HandlePlayerLogin(LoginQueryHolder * holder);
         void HandleCharRenameOpcode(WorldPacket& recvData);
         void HandleCharRenameCallBack(PreparedQueryResult result, WorldPackets::Character::CharacterRenameInfo const* renameInfo);
@@ -568,7 +569,7 @@ class WorldSession
         void HandleMeetingStoneInfo(WorldPacket& recPacket);
         void HandleGameobjectReportUse(WorldPacket& recvPacket);
 
-        void HandleNameQueryOpcode(WorldPacket& recvPacket);
+        void HandleNameQueryOpcode(WorldPackets::Character::QueryPlayerName& queryPlayerName);
 
         void HandleQueryTimeOpcode(WorldPacket& recvPacket);
 

Declipe/TrinityCore

(cherry picked from commit ec7923c2e6f7778d82e8102d38cd54e0827fb840)

src/server/game/Server/WorldSession.h
@@ -230,6 +230,7 @@ namespace WorldPackets
     namespace Query
     {
         class QueryCreature;
+        class QueryPlayerName;
         class QueryGameObject;
         class QueryCorpseLocationFromClient;
         class QueryCorpseTransport;
@@ -813,7 +814,7 @@ class TC_GAME_API WorldSession
         void HandleGameObjectUseOpcode(WorldPacket& recPacket);
         void HandleGameobjectReportUse(WorldPacket& recvPacket);
 
-        void HandleNameQueryOpcode(WorldPacket& recvPacket);
+        void HandleNameQueryOpcode(WorldPackets::Query::QueryPlayerName& queryPlayerName);
 
         void HandleQueryTimeOpcode(WorldPacket& recvPacket);
 