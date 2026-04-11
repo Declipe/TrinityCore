13dd2885ba8506344d400dc91525db7fc24d55fd
ccrs <ccrs@users.noreply.github.com>
2026-04-11 15:55:41 +0200

    Core/Entities: force CombatManager revalidation after applying charm
    "Safely" goes through all combat references validating if in fact the unit should remain in combat or not
    Any edge case that may get affected by this, are scenarios in which the charmed unit must keep the combat and threat references prior to their charm.... I can't think of any.
    -> And honestly if there was any such case... imo it should be handled script wise, not core wise.
    
    closes #31747
    updates #30169

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -11602,6 +11602,7 @@ bool Unit::SetCharmedBy(Unit* charmer, CharmType type, AuraApplication const* au
 
     // Set charmed
     charmer->SetCharm(this, true);
+    m_combatManager.RevalidateCombat();
 
     if (Player* player = ToPlayer())
     {
