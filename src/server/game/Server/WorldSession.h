eeeb3baf4d633ccdbeb33dc8f790d960de8071f3
Declipe <Declipe@yandex.ru>
2026-05-11 08:30:44 +0300

    part3

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -210,6 +210,7 @@ namespace WorldPackets
 
     namespace Misc
     {
+        class TimeSyncResponse;
         class CompleteCinematic;
         class CompleteMovie;
         class NextCinematicCamera;
@@ -1068,7 +1069,7 @@ class TC_GAME_API WorldSession
         void HandleSetRaidDifficultyOpcode(WorldPacket& recvData);
         void HandleSetTitleOpcode(WorldPacket& recvData);
         void HandleRealmSplitOpcode(WorldPacket& recvData);
-        void HandleTimeSyncResponse(WorldPacket& recvData);
+        void HandleTimeSyncResponse(WorldPackets::Misc::TimeSyncResponse& packet);
         void HandleWhoIsOpcode(WorldPacket& recvData);
         void HandleResetInstancesOpcode(WorldPacket& recvData);
         void HandleHearthAndResurrect(WorldPacket& recvData);
