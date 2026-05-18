e0086c88b92efd64b0b12ab83e652384eb3e392b
Declipe <Declipe@yandex.ru>
2026-05-18 19:42:02 +0300

    Revert "test update Core/Spell: Cherry Pick Rename SPELL_ATTR2_HEALTH_FUNNEL to SPELL_ATTR2_NO_TARGET_PER_SECOND"
    This reverts commit ac47578983919450e94a9a2bbd7159c14e4c4677.
    
    Revert "implement Spell Queue System"
    
    This reverts commit d3cac8b98bc112963a92fbd5dea066d1e80a8637.
    
    # Conflicts:
    #	src/server/game/Handlers/SpellHandler.cpp

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -5341,6 +5341,10 @@ void AuraEffect::HandlePeriodicHealAurasTick(Unit* target, Unit* caster) const
         return;
     }
 
+    // heal for caster damage (must be alive)
+    if (target != caster && GetSpellInfo()->HasAttribute(SPELL_ATTR2_HEALTH_FUNNEL) && (!caster || !caster->IsAlive()))
+        return;
+
     // don't regen when permanent aura target has full power
     if (GetBase()->IsPermanent() && target->IsFullHealth())
         return;
@@ -5382,6 +5386,32 @@ void AuraEffect::HandlePeriodicHealAurasTick(Unit* target, Unit* caster) const
         target->GetThreatManager().ForwardThreatForAssistingMe(caster, healInfo.GetEffectiveHeal() * 0.5f, GetSpellInfo());
 
     bool haveCastItem = !GetBase()->GetCastItemGUID().IsEmpty();
+
+    // Health Funnel
+    // damage caster for heal amount
+    if (target != caster && GetSpellInfo()->HasAttribute(SPELL_ATTR2_HEALTH_FUNNEL))
+    {
+        uint32 funnelDamage = GetSpellInfo()->ManaPerSecond; // damage is not affected by spell power
+
+        if (funnelDamage > healInfo.GetEffectiveHeal() && healInfo.GetEffectiveHeal())
+            funnelDamage = healInfo.GetEffectiveHeal();
+
+        uint32 funnelAbsorb = 0;
+        Unit::DealDamageMods(caster, funnelDamage, &funnelAbsorb);
+
+        if (caster)
+        {
+           SpellNonMeleeDamage log(caster, caster, GetId(), GetSpellInfo()->GetSchoolMask());
+           log.damage = funnelDamage;
+           log.absorb = funnelAbsorb;
+           log.periodicLog = true;
+           caster->SendSpellNonMeleeDamageLog(&log);
+
+        }
+
+        CleanDamage cleanDamage = CleanDamage(0, 0, BASE_ATTACK, MELEE_HIT_NORMAL);
+        Unit::DealDamage(caster, caster, funnelDamage, &cleanDamage, SELF_DAMAGE, GetSpellInfo()->GetSchoolMask(), GetSpellInfo(), true);
+    }
 
     // %-based heal - does not proc auras
     if (GetAuraType() == SPELL_AURA_OBS_MOD_HEALTH)
--- src/server/shared/Packets/ByteBuffer.h
+++ src/server/shared/Packets/ByteBuffer.h
@@ -390,8 +390,6 @@ class TC_SHARED_API ByteBuffer
             }
         }
 
-        uint32 ReadPackedTime();
-
         std::string ReadCString(bool requireValidUtf8 = true);
 
         uint8* contents()
