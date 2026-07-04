bca72df9b298b706fd838d78b9c76bac255fb602
Declipe <Declipe@yandex.ru>
2026-07-04 07:49:16 +0300

    test

--- src/server/scripts/Custom/Player/PlayerScripts.cpp
+++ src/server/scripts/Custom/Player/PlayerScripts.cpp
@@ -145,8 +145,160 @@ private:
 //    }
 //};
 
+using namespace Trinity::ChatCommands;
+
+class bg_queue_commandscript : public CommandScript
+{
+public:
+    bg_queue_commandscript() : CommandScript("bg_queue_commandscript") {}
+
+    std::vector<ChatCommand> GetCommands() const override
+    {
+        static std::vector<ChatCommand> bgqCommandTable =
+        {
+            { "queue",    HandleBgQueueCommand,    rbac::RBAC_PERM_COMMAND_GM, Console::Yes },
+            { "leave",    HandleBgLeaveCommand,    rbac::RBAC_PERM_COMMAND_GM, Console::Yes },
+            { "window",  HandleBgWindowCommand,    rbac::RBAC_PERM_COMMAND_GM, Console::Yes },
+            //{ "",  HandleBgWindowCommand,    rbac::RBAC_PERM_COMMAND_GM, Console::Yes },
+        };
+
+        static std::vector<ChatCommand> commandTable =
+        {
+            { "rgb", bgqCommandTable },
+        };
+        return commandTable;
+    }
+
+    static bool HandleBgWindowCommand(ChatHandler* handler, char const* args)
+    {
+        WorldSession* session = handler->GetSession();
+        Player* player = handler->GetSession()->GetPlayer();
+
+        uint32 bgId = args && *args ? atoi(args) : 2;
+
+        switch (bgId)
+        {
+        case 1:  // AV
+        case 2:  // WSG
+        case 3:  // AB
+        case 7:  // EotS
+        case 9:  // SotA
+        case 30: // IoC
+            break; //NOT_USED_73
+        default:
+            handler->SendSysMessage("1=AV, 2=WSG, 3=AB, 7=EotS, 9=SotA, 30=IoC");
+            return true;
+        }
+
+        BattlegroundTypeId bgTypeId = BattlegroundTypeId(bgId);
+
+        if (DisableMgr::IsDisabledFor(DISABLE_TYPE_BATTLEGROUND, bgTypeId, nullptr))
+        {
+            handler->SendSysMessage(session->GetTrinityString2(NOT_USED_73));
+            return true;
+        }
+
+        player->GetSession()->SendBattleGroundList(player->GetGUID(), bgTypeId);
+        return true;
+    }
+
+    static bool HandleBgQueueCommand(ChatHandler* handler, char const* args)
+    {
+        WorldSession* session = handler->GetSession();
+        Player* player = handler->GetSession()->GetPlayer();
+
+        // ID ??: 1=AV, 2=WSG, 3=AB, 7=EotS, 9=SotA, 30=IoC
+        uint32 bgId = args && *args ? atoi(args) : 2;
+        switch (bgId)
+        {
+        case 1:
+        case 2:
+        case 3:
+        case 7:
+        case 9:
+        case 30:
+            break;
+        default:
+            handler->SendSysMessage("1=AV, 2=WSG, 3=AB, 7=EotS, 9=SotA, 30=IoC");
+            return true;
+        }
+        BattlegroundTypeId bgTypeId = BattlegroundTypeId(bgId);
+
+        Battleground* bg = sBattlegroundMgr->GetBattlegroundTemplate(bgTypeId);
+        if (!bg)
+        {
+            handler->SendSysMessage("1=AV, 2=WSG, 3=AB, 7=EotS, 9=SotA, 30=IoC");
+            return true;
+        }
+
+        if (DisableMgr::IsDisabledFor(DISABLE_TYPE_BATTLEGROUND, bgTypeId, nullptr))
+        {
+            handler->SendSysMessage(session->GetTrinityString2(NOT_USED_73));
+            return true;
+        }
+
+        if (player->InBattleground() || player->InBattlegroundQueue())
+        {
+            handler->SendSysMessage("?.");
+            return true;
+        }
+
+        if (player->HasAura(26013))
+        {
+            handler->SendSysMessage("?.");
+            return true;
+        }
+
+        PvPDifficultyEntry const* bracketEntry = GetBattlegroundBracketByLevel(bg->GetMapId(), player->GetLevel());
+        if (!bracketEntry)
+            bracketEntry = GetBattlegroundBracketById(bg->GetMapId(), BG_BRACKET_ID_FIRST);
+
+        if (!bracketEntry)
+        {
+            handler->SendSysMessage(session->GetTrinityString2(NOT_USED_73));
+            return true;
+        }
+
+        BattlegroundQueueTypeId bgQueueTypeId = BattlegroundMgr::BGQueueTypeId(bgTypeId, 0);
+        BattlegroundQueue& bgQueue = sBattlegroundMgr->GetBattlegroundQueue(bgQueueTypeId);
+
+        GroupQueueInfo* ginfo = bgQueue.AddGroup(player, nullptr, bgTypeId, bracketEntry, 0, false, false, 0, 0);
+        uint32 avgTime = bgQueue.GetAverageQueueWaitTime(ginfo, bracketEntry->GetBracketId());
+        uint32 queueSlot = player->AddBattlegroundQueueId(bgQueueTypeId);
+
+        WorldPacket data;
+        sBattlegroundMgr->BuildBattlegroundStatusPacket(&data, bg, queueSlot, STATUS_WAIT_QUEUE, avgTime, 0, 0, 0);
+        player->GetSession()->SendPacket(&data);
+
+        sBattlegroundMgr->ScheduleQueueUpdate(0, 0, bgQueueTypeId, bgTypeId, bracketEntry->GetBracketId());
+
+        handler->PSendSysMessage("?? ?????? ? ???????: %s", bg->GetName());
+        return true;
+    }
+
+    static bool HandleBgLeaveCommand(ChatHandler* handler, char const* /*args*/)
+    {
+        Player* player = handler->GetSession()->GetPlayer();
+
+        for (uint8 i = 0; i < PLAYER_MAX_BATTLEGROUND_QUEUES; ++i)
+        {
+            BattlegroundQueueTypeId qId = player->GetBattlegroundQueueTypeId(i);
+            if (qId == BATTLEGROUND_QUEUE_NONE)
+                continue;
+
+            BattlegroundQueue& queue = sBattlegroundMgr->GetBattlegroundQueue(qId);
+            queue.RemovePlayer(player->GetGUID(), true);
+            player->RemoveBattlegroundQueueId(qId);
+        }
+
+        handler->SendSysMessage("?.");
+        return true;
+    }
+};
+
 void AddSC_ZynPlayerScripts()
 {
+    new bg_queue_commandscript();
     new buff_zones();
     new item_lvlup();
     new lfg_solo_announce();
