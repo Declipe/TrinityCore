5a4610d830db9a564fd1fa31e3aa7e8d1d42818f
Declipe <Declipe@yandex.ru>
2026-07-05 09:01:44 +0300

    up merge

--- src/server/game/Server/Packets/AllPackets.h
+++ src/server/game/Server/Packets/AllPackets.h
@@ -19,6 +19,7 @@
 #define TRINITYCORE_ALL_PACKETS_H
 
 #include "AchievementPackets.h"
+#include "AreaTriggerPackets.h"
 #include "AuthenticationPackets.h"
 #include "BankPackets.h"
 #include "BattlefieldPackets.h"
