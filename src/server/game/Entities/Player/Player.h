b14bbac21259a70e37cb3d3d854536d59c87c07d
Declipe <Declipe@yandex.ru>
2026-07-06 08:04:46 +0300

    merge tc

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1637,8 +1637,8 @@ public:
     uint32 GetMovie() const { return m_movie; }
     void SetMovie(uint32 movie) { m_movie = movie; }
 
-    ActionButton* addActionButton(uint8 button, uint32 action, uint8 type);
-    void removeActionButton(uint8 button);
+    ActionButton* AddActionButton(uint8 button, uint32 action, uint8 type);
+    void RemoveActionButton(uint8 button);
     ActionButton const* GetActionButton(uint8 button);
     void SendInitialActionButtons() const { SendActionButtons(1); }
     void SendActionButtons(uint32 state) const;
