4cb8a207675960205615071d32fa9c70b7eb5cc0
Declipe <Declipe@yandex.ru>
2026-07-09 04:43:38 +0300

    up

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -353,9 +353,7 @@ Player::Player(WorldSession* session) : Unit(true)
     isDebugAreaTriggers = false;
 
     m_WeeklyQuestChanged = false;
-
     m_MonthlyQuestChanged = false;
-
     m_SeasonalQuestChanged = false;
 
     SetPendingBind(0, 0);
@@ -18789,10 +18787,8 @@ void Player::_LoadQuestStatusRewarded(PreparedQueryResult result)
 
                 // set rewarded title if any
                 if (quest->GetRewTitle())
-                {
                     if (CharTitlesEntry const* titleEntry = sDBCMgr->GetCharTitlesEntry(quest->GetRewTitle()))
                         SetTitle(titleEntry);
-                }
 
                 if (uint32 talents = quest->GetBonusTalents())
                     AddQuestRewardedTalentCount(talents);
@@ -22932,9 +22928,6 @@ void Player::SendInitialPacketsBeforeAddToMap()
 
     /// SMSG_SEND_UNLEARN_SPELLS
     SendUnlearnSpells();
-
-    /// @todo: SMSG_SEND_SPELL_HISTORY
-    /// @todo: SMSG_SEND_SPELL_CHARGES
 
     /// SMSG_ACTION_BUTTONS
     SendInitialActionButtons();
