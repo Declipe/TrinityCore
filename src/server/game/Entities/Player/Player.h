6ed77d30a7235dfb1e145e53796dface653e0d40
Declipe <Declipe@yandex.ru>
2026-07-24 20:21:25 +0300

    merge tc

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2141,7 +2141,7 @@ public:
     // only changed for direct client control (possess, vehicle etc.), not stuff you control using pet commands
     WorldObject* m_seer;
     void SetFallInformation(uint32 time, float z);
-    void HandleFall(MovementInfo const& movementInfo);
+    void HandleFall();
 
     bool CanFlyInZone(uint32 mapid, uint32 zone, SpellInfo const* bySpell) const;
 
